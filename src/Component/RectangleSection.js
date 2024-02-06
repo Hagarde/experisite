f74dcbc90aa16b13f47ed7f5a1507603c7a0adaf
Hagarde <43098624+Hagarde@users.noreply.github.com>
2024-02-06 17:24:59 +0100

    quetionnaire et otut

--- src/Component/RectangleSection.js
+++ src/Component/RectangleSection.js
@@ -7,7 +7,7 @@ const Rectangle = (regionName) => {
     // Récupérez la valeur du store Redux pour la hauteur des rectangles
     const rectangleHeight = useSelector((state) => { return {'XA': state.game.frame.XA,'XB': state.game.frame.XB,'YA': state.game.frame.YA,'YB': state.game.frame.YB}});
     return (
-      <div class=''style={{ height:'50%'}}>
+      <div class=''style={{ height:'50%', display: 'flex', alignItems: 'flex-end'}}>
         <div 
           style={{ height: rectangleHeight[regionName]*200+'px', background: regionStyle[regionName].background, marginBottom: '1px'}}>
           <div style={titleStyle}>{regionName}</div>
--- src/page/Questionnaire.js
+++ src/page/Questionnaire.js
@@ -2,27 +2,22 @@ import Form from 'react-bootstrap/Form';
 import React from "react";
 
 const Questionnaire = () => {
-return (
-    <div>
-        <Form.Label htmlFor="inputPassword5"> Votre avis sur la longueur de l'expérience </Form.Label>
-        <Form.Control
-            type="text"
-            aria-describedby="passwordHelpBlock"
-        />
-        <Form.Text id="passwordHelpBlock" muted>
+    return (
+        <div>
 
-        </Form.Text>
-        <Form.Label htmlFor="inputPassword5"> Votre avis sur la clareté </Form.Label>
-        <Form.Control
-            type="text"
-            aria-describedby="passwordHelpBlock"
-        />
-        <Form.Text id="passwordHelpBlock" muted>
+            <Form.Label>À quel point avez-vous aimez cette expérience ? </Form.Label>
+            <Form.Range />
+            <Form.Label htmlFor="inputPassword5"> Votre avis sur la clareté </Form.Label>
+            <Form.Control
+                type="text"
+                aria-describedby="passwordHelpBlock"
+            />
+            <Form.Text id="passwordHelpBlock" muted>
 
-        </Form.Text>
-        <button type="submit"> Envoyer </button>
-    </div>
-)
+            </Form.Text>
+            <button type="submit"> Envoyer </button>
+        </div>
+    )
 }
 
 export default Questionnaire;
